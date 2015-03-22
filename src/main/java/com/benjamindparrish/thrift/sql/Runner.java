ba07622ffc7bf207a0aff01ec0b90cc3e3460696
Benjamin Parrish <ben@thehumangeo.com>
2015-03-22 18:54:33 -0400

    - Initial project setup ... again

--- src/main/java/com/benjamindparrish/thrift/sql/Runner.java
+++ src/main/java/com/benjamindparrish/thrift/sql/Runner.java
@@ -4,4 +4,13 @@ package com.benjamindparrish.thrift.sql;
  * Created by bparrish on 3/22/15.
  */
 public class Runner {
+
+    public Runner() {
+        System.out.println("Test");
+    }
+
+    public static void main(String... args) {
+        Runner runner = new Runner();
+    }
+
 }
